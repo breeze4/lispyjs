989f6f4e8fdb9a06f5cbb13180e9f68e4c4670f0
breeze4 <ambiturner0@gmail.com>
2015-11-08 12:17:38 -0800

    added comparison operators and subtraction/division

--- src/environment.js
+++ src/environment.js
@@ -3,7 +3,7 @@ var env = {};
 function standardEnv() {
     env = {
         '+': function () {
-            if (!arguments || arguments.length == 0) return null;
+            if (!arguments || arguments.length == 0) throw new SyntaxError('invalid arity of args');
             if (arguments.length == 1) return arguments[0];
             var runSum = 0;
             for (var i = 0; i < arguments.length; i++) {
@@ -11,14 +11,82 @@ function standardEnv() {
             }
             return runSum;
         },
+        '-': function () {
+            if (!arguments || arguments.length == 0) throw new SyntaxError('invalid arity of args');
+            if (arguments.length == 1) return -arguments[0];
+            var runSum = arguments[0];
+            for (var i = 1; i < arguments.length; i++) {
+                runSum -= arguments[i];
+            }
+            return runSum;
+        },
         '*': function () {
-            if (!arguments || arguments.length == 0) return null;
+            if (!arguments || arguments.length == 0) throw new SyntaxError('invalid arity of args');
             if (arguments.length == 1) return arguments[0];
             var runProduct = arguments[0];
             for (var i = 1; i < arguments.length; i++) {
                 runProduct *= arguments[i];
             }
             return runProduct;
+        },
+        '/': function () {
+            if (!arguments || arguments.length == 0) throw new SyntaxError('invalid arity of args');
+            if (arguments.length == 1) return 1 / arguments[0];
+            var runQuotient = arguments[0];
+            for (var i = 1; i < arguments.length; i++) {
+                runQuotient /= arguments[i];
+            }
+            return runQuotient;
+        },
+        '=': function () {
+            if (!arguments || arguments.length == 0) throw new SyntaxError('invalid arity of args');
+            var previous = arguments[0], current;
+            for (var i = 1; i < arguments.length; i++) {
+                current = arguments[i];
+                if (current === previous) previous = current;
+                else return false;
+            }
+            return true;
+        },
+        '<': function () {
+            if (!arguments || arguments.length == 0) throw new SyntaxError('invalid arity of args');
+            var previous = arguments[0], current;
+            for (var i = 1; i < arguments.length; i++) {
+                current = arguments[i];
+                if (current > previous) previous = current;
+                else return false;
+            }
+            return true;
+        },
+        '>': function () {
+            if (!arguments || arguments.length == 0) throw new SyntaxError('invalid arity of args');
+            var previous = arguments[0], current;
+            for (var i = 1; i < arguments.length; i++) {
+                current = arguments[i];
+                if (current < previous) previous = current;
+                else return false;
+            }
+            return true;
+        },
+        '<=': function () {
+            if (!arguments || arguments.length == 0) throw new SyntaxError('invalid arity of args');
+            var previous = arguments[0], current;
+            for (var i = 1; i < arguments.length; i++) {
+                current = arguments[i];
+                if (current >= previous) previous = current;
+                else return false;
+            }
+            return true;
+        },
+        '>=': function () {
+            if (!arguments || arguments.length == 0) throw new SyntaxError('invalid arity of args');
+            var previous = arguments[0], current;
+            for (var i = 1; i < arguments.length; i++) {
+                current = arguments[i];
+                if (current <= previous) previous = current;
+                else return false;
+            }
+            return true;
         }
     };
 
--- test/interpreter.spec.js
+++ test/interpreter.spec.js
@@ -46,5 +46,99 @@ describe('Interpreter', function () {
                 });
             });
         });
+        describe('subtract', function () {
+            describe('directly called', function () {
+                it('on pair of numbers', function () {
+                    assert.equal(env['-'](2, 3), -1);
+                });
+                it('on list of numbers', function () {
+                    assert.equal(env['-'].apply(null, [12, 3, 4]), 5);
+                });
+            });
+            describe('called by eval', function () {
+                it('on pair of numbers', function () {
+                    assert.equal(interpreter.eval(parser.parse('(- 2 3)'), env), -1);
+                    assert.equal(interpreter.eval(parser.parse('(- 12 (- 4 -3))'), env), 5);
+                    assert.equal(interpreter.eval(parser.parse('(- (- 12 5) (- 4 3))'), env), 6);
+                });
+                it('on list of numbers', function () {
+                    assert.equal(interpreter.eval(parser.parse('(- (- 120 5 2) (- 14 3 1) (- 14 3 2))'), env), 94);
+                });
+            });
+        });
+        describe('division', function () {
+            describe('directly called', function () {
+                it('on pair of numbers', function () {
+                    assert.equal(env['/'](6, 3), 2);
+                });
+                it('on list of numbers', function () {
+                    assert.equal(env['/'].apply(null, [12, 4, 3]), 1);
+                });
+            });
+            describe('called by eval', function () {
+                it('on pair of numbers', function () {
+                    assert.equal(interpreter.eval(parser.parse('(/ 6 -3)'), env), -2);
+                    assert.equal(interpreter.eval(parser.parse('(/ 12 (/ 6 -3))'), env), -6);
+                    assert.equal(interpreter.eval(parser.parse('(/ (/ 12 4) (/ 3 -3))'), env), -3);
+                });
+                it('on list of numbers', function () {
+                    assert.equal(interpreter.eval(parser.parse('(/ (/ 120 6 2) (/ 14 7 1) (/ 12 3 2))'), env), 2.5);
+                });
+            });
+        });
+    });
+    describe('comparison operators', function () {
+        describe('less than', function () {
+            it('should return true for strictly increasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(< 5 13 16)'), env), true);
+            });
+            it('should return false for decreasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(< 5 4 3)'), env), false);
+            });
+            it('should return false for increasing and then decreasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(< 5 14 7)'), env), false);
+            });
+        });
+        describe('greater than', function () {
+            it('should return true for strictly decreasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(> 5 4 3)'), env), true);
+            });
+            it('should return false for increasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(> 5 7 9)'), env), false);
+            });
+            it('should return false for decreasing and then increasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(> 5 3 4)'), env), false);
+            });
+        });
+        describe('less than or equal', function () {
+            it('should return true for strictly increasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(<= 5 5 13 16)'), env), true);
+            });
+            it('should return false for decreasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(<= 5 4 3)'), env), false);
+            });
+            it('should return false for increasing and then decreasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(<= 5 14 7)'), env), false);
+            });
+        });
+        describe('greater than or equal', function () {
+            it('should return true for strictly decreasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(>= 5 5 4 4 3)'), env), true);
+            });
+            it('should return false for increasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(>= 5 5 7 9)'), env), false);
+            });
+            it('should return false for decreasing and then increasing numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(>= 5 5 3 4)'), env), false);
+            });
+        });
+        describe('equals', function () {
+            it('should return true for the same numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(= 5 5 5)'), env), true);
+            });
+            it('should return false for different numbers', function () {
+                assert.equal(interpreter.eval(parser.parse('(= 5 5 3 5)'), env), false);
+            });
+        });
     });
 });
